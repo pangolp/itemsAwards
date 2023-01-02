cf84622ab284e0f7138e150f38912b04ae9c9d4a
Pagani Walter <paganiwalter@gmail.com>
2023-01-02 19:20:02 -0300

    Add multiple items
    By changing the method, and adding an extra parameter to the table, more than one item can be given to the player.

--- media/lua/client/awards.lua
+++ media/lua/client/awards.lua
@@ -1,10 +1,10 @@
 local itemsAwards = {
-  { Item = "Base.BaseballBatNails", Kills = 1 },
-  { Item = "Base.BaseballBatNails", Kills = 2 },
-  { Item = "Base.Trousers_CamoGreen", Kills = 10 },
-  { Item = "Base.Shoes_ArmyBoots", Kills = 15 },
-  { Item = "Base.Vest_BulletArmy", Kills = 20 },
-  { Item = "Base.Tshirt_CamoGreen", Kills = 25 }
+  { Item = "Base.BaseballBatNails", Kills = 1, Count = 1 },
+  { Item = "Base.Money", Kills = 2, Count = 10 },
+  { Item = "Base.Trousers_CamoGreen", Kills = 10, Count = 1 },
+  { Item = "Base.Shoes_ArmyBoots", Kills = 15, Count = 1 },
+  { Item = "Base.Vest_BulletArmy", Kills = 20, Count = 1 },
+  { Item = "Base.Tshirt_CamoGreen", Kills = 25, Count = 1 }
 }
 
 local function ZombKilled()
@@ -13,8 +13,8 @@ local function ZombKilled()
   for key, value in pairs(itemsAwards) do
     if ( countZombieKill == value.Kills ) then
       local itemName = getItemNameFromFullType(value.Item)
-      player:Say(string.format("You have won: %s", itemName))
-      player:getInventory():AddItem(value.Item)
+      player:Say(string.format("You have won: %s. Count: %d", itemName, value.Count))
+      player:getInventory():AddItems(value.Item, value.Count)
     end
   end
   player:Say(string.format("[Items Awards]: Zombies killed: %d", countZombieKill))
