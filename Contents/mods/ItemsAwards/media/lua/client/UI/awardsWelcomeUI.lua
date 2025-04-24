cbab2b413b0504ccc07510fcc71c68600135282a
ElTurcoJunior <gchivilo@gmail.com>
2025-04-24 15:45:57 -0300

    Se agregaron iconos al historial de items ganados y por algun motivo tambien ahora se autorecolecctan en el inventario

---
+++ Contents/mods/ItemsAwards/media/lua/client/UI/awardsWelcomeUI.lua
@@ -0,0 +1,250 @@
+require "ISUI/ISPanel"
+require "ISUI/ISButton"
+require "ISUI/ISScrollingListBox"
+
+awardsWelcomeWindow = nil
+
+AwardsWelcomeUI = ISPanel:derive("AwardsWelcomeUI")
+
+function AwardsWelcomeUI:initialise()
+    ISPanel.initialise(self)
+    self:create()
+end
+
+function AwardsWelcomeUI:prerender()
+    ISPanel.prerender(self)
+    self:drawText(getText("UI_welcomeawards"), 10, 10, 1, 1, 1, 1, UIFont.Medium)
+    self:drawText(getText("UI_version") .. ": 1.0", 10, 40, 0.8, 0.8, 0.8, 1, UIFont.Small)
+    self:drawText(getText("UI_instructions"), 10, 70, 0.8, 0.8, 0.8, 1, UIFont.Small)
+end
+
+function AwardsWelcomeUI:create()
+    local btnWidth = 100
+    local btnHeight = 25
+
+    self.awardsList = ISScrollingListBox:new(10, 100, self.width - 20, 100)
+    self.awardsList:initialise()
+    self.awardsList:instantiate()
+    self.awardsList.itemheight = 32 -- Alto suficiente para íconos grandes
+    self.awardsList.selected = 0
+    self.awardsList.joypadParent = self
+    self.awardsList.font = UIFont.NewSmall
+    self.awardsList.doDrawItem = self.drawAwardItem
+    self.awardsList:setOnMouseDownFunction(self, self.onAwardClick)
+    self:addChild(self.awardsList)
+
+    self.losersList = ISScrollingListBox:new(10, self.awardsList:getY() + self.awardsList:getHeight() + 10, self.width - 20, 100)
+    self.losersList:initialise()
+    self.losersList:instantiate()
+    self.losersList.itemheight = 20
+    self.losersList.selected = 0
+    self.losersList.font = UIFont.NewSmall
+    self.losersList.doDrawItem = self.drawLoserItem
+    self:addChild(self.losersList)
+
+    self.closeButton = ISButton:new(
+        self.width - 490,
+        self.losersList:getY() + self.losersList:getHeight() + 10,
+        btnWidth,
+        btnHeight,
+        getText("UI_Close"),
+        self,
+        AwardsWelcomeUI.onCloseClick
+    )
+    self:addChild(self.closeButton)
+
+    self.cleanButton = ISButton:new(
+        self.closeButton:getX() + btnWidth + 10,
+        self.losersList:getY() + self.losersList:getHeight() + 10,
+        btnWidth,
+        btnHeight,
+        getText("UI_clean"),
+        self,
+        AwardsWelcomeUI.onCleanClick
+    )
+    self:addChild(self.cleanButton)
+
+    self.cleanLoserButton = ISButton:new(
+        self.cleanButton:getX() + btnWidth + 10,
+        self.losersList:getY() + self.losersList:getHeight() + 10,
+        btnWidth,
+        btnHeight,
+        getText("UI_clean_loser"),
+        self,
+        AwardsWelcomeUI.onCleanLoserClick
+    )
+    self:addChild(self.cleanLoserButton)
+end
+
+function AwardsWelcomeUI:drawAwardItem(y, item, alt)
+    local a = 0.9
+    self:drawRectBorder(0, y, self:getWidth(), self.itemheight - 1, a, self.borderColor.r, self.borderColor.g, self.borderColor.b)
+
+    if self.selected == item.index then
+        self:drawRect(0, y, self:getWidth(), self.itemheight - 1, 0.3, 0.7, 0.35, 0.15)
+    end
+
+    -- Ícono la mitad de grande
+    local iconSize = (self.itemheight - 4) / 2
+    local x = 5
+
+    if item.item and item.item.icon then
+        self:drawTextureScaledAspect(item.item.icon, x, y + (self.itemheight - iconSize) / 2, iconSize, iconSize, a, 1, 1, 1)
+    end
+
+    local nameX = x + iconSize + 8
+    if item.item and item.item.name then
+        self:drawText(item.item.name, nameX, y + 6, 1, 1, 1, a, self.font)
+    end
+
+    if item.item and item.item.count and tostring(item.item.count) ~= "" then
+        local countText = "x" .. tostring(item.item.count)
+        local countX = self:getWidth() - 40 - getTextManager():MeasureStringX(self.font, countText)
+        self:drawText(countText, countX, y + 6, 1, 1, 1, a, self.font)
+    end
+
+    return y + self.itemheight
+end
+
+function AwardsWelcomeUI:drawLoserItem(y, item, alt)
+    local a = 0.9
+    self:drawRectBorder(0, y, self:getWidth(), self.itemheight - 1, a, 0.5, 0, 0)
+    self:drawText(item.text, 10, y + 2, 1, 1, 1, a, self.font)
+    return y + self.itemheight
+end
+
+function AwardsWelcomeUI:onAwardClick(item) end
+
+function AwardsWelcomeUI:onCloseClick()
+    self:setVisible(false)
+    self:removeFromUIManager()
+end
+
+function AwardsWelcomeUI:onCleanClick()
+    self.awardsList:clear()
+end
+
+function AwardsWelcomeUI:onCleanLoserClick()
+    self.losersList:clear()
+end
+
+function AwardsWelcomeUI:addAwardMessage(itemFullType, message)
+    local icon, name, count = nil, message, ""
+    if itemFullType then
+        local item = InventoryItemFactory.CreateItem(itemFullType)
+        if item then
+            icon = item:getTex()
+            name = item:getDisplayName()
+        end
+    end
+    local qty = string.match(message, "x(%d+)")
+    if qty then count = qty end
+    self.awardsList:insertItem(1, name, {icon = icon, name = name, count = count})
+    self.awardsList.selected = 1
+end
+
+function AwardsWelcomeUI:addLoserMessage(message)
+    local limit = Awards.Options.limitLosingNumbers * 5
+    self.losersList:insertItem(1, message, {})
+    self.losersList.selected = 1
+    while self.losersList:size() > limit do
+        self.losersList:removeItemByIndex(self.losersList:size())
+    end
+end
+
+function AwardsWelcomeUI:new(x, y, width, height)
+    local o = {}
+    o = ISPanel:new(x, y, width, height)
+    setmetatable(o, self)
+    self.__index = self
+
+    o.backgroundColor = {r=0.1, g=0.1, b=0.1, a=0.9}
+    o.borderColor = {r=0.7, g=0.7, b=0.7, a=0.5}
+    o.moveWithMouse = true
+    return o
+end
+
+function CreateWelcomeWindow()
+    if awardsWelcomeWindow then return end
+
+    local screenW = getCore():getScreenWidth()
+    local screenH = getCore():getScreenHeight()
+    local width = 500
+    local height = 380
+    local x = (screenW - width) / 2 + 400
+    local y = (screenH - height) / 2
+
+    awardsWelcomeWindow = AwardsWelcomeUI:new(x, y, width, height)
+    awardsWelcomeWindow:initialise()
+    awardsWelcomeWindow:addToUIManager()
+    awardsWelcomeWindow:setVisible(false)
+end
+
+AwardsHUDButton = ISButton:derive("AwardsHUDButton")
+AwardsHUDButton.instance = nil
+
+function AwardsHUDButton:new(x, y, width, height)
+    local o = ISButton:new(x, y, width, height, "", nil, function()
+        if awardsWelcomeWindow and awardsWelcomeWindow:isVisible() then
+            awardsWelcomeWindow:setVisible(false)
+            awardsWelcomeWindow:removeFromUIManager()
+        else
+            if not awardsWelcomeWindow then
+                CreateWelcomeWindow()
+            else
+                awardsWelcomeWindow:setVisible(true)
+                awardsWelcomeWindow:addToUIManager()
+            end
+        end
+    end)
+
+    setmetatable(o, self)
+    self.__index = self
+
+    o:setImage(getTexture("media/ui/icons/gift_regular_icon.png"))
+    o.backgroundColor = {r=0, g=0, b=0, a=0}
+    o.backgroundColorMouseOver = {r=1, g=1, b=1, a=0.1}
+    o.borderColor = {r=0, g=0, b=0, a=0}
+    return o
+end
+
+local function createHUDButton()
+    if AwardsHUDButton.instance then return end
+    local btnSize = 32
+    local x = getCore():getScreenWidth() - btnSize - 80
+    local y = 100
+
+    local btn = AwardsHUDButton:new(x, y, btnSize, btnSize)
+    btn:setAnchorLeft(false)
+    btn:setAnchorRight(true)
+    btn:setAnchorTop(true)
+    btn:setAnchorBottom(false)
+    btn.tooltip = getText("UI_awards_button_tooltip")
+    btn:initialise()
+    btn:addToUIManager()
+    AwardsHUDButton.instance = btn
+end
+
+local function OnGameStart()
+    Events.OnTick.Add(function()
+        if not awardsWelcomeWindow then
+            CreateWelcomeWindow()
+            Events.OnTick.Remove(this)
+        end
+    end)
+    createHUDButton()
+end
+
+Events.OnGameStart.Add(OnGameStart)
+
+function AddAwardMessageToUI(itemFullType, message)
+    if awardsWelcomeWindow then
+        awardsWelcomeWindow:addAwardMessage(itemFullType, message)
+    end
+end
+
+function AddLoserMessageToUI(message)
+    if awardsWelcomeWindow then
+        awardsWelcomeWindow:addLoserMessage(message)
+    end
+end
--- Contents/mods/ItemsAwards/media/lua/client/awards.lua
+++ Contents/mods/ItemsAwards/media/lua/client/awards.lua
@@ -2,96 +2,133 @@ Awards = Awards or {}
 Awards.Options = Awards.Options or {}
 
 local itemsAwards = {
-  {Item = "Base.BaseballBatNails", Number = 5, Count = 1}, -- Spiked Baseball Bat
-  {Item = "Base.FishingRodBreak", Number = 10, Count = 1}, -- Fishing Rod without Line
-  {Item = "Base.MetalBar", Number = 15, Count = 3}, -- Metal Bar
-  {Item = "Base.MetalPipe", Number = 20, Count = 3}, -- Metal Pipe
-  {Item = "Base.Katana", Number = 25, Count = 1}, -- Katana
-  {Item = "Base.Machete", Number = 30, Count = 1}, -- Machete
-  {Item = "Base.GardenFork", Number = 35, Count = 1}, -- Garden Fork
-  {Item = "Base.Revolver_Short", Number = 40, Count = 1}, -- M36 Revolver
-  {Item = "Base.AssaultRifle2", Number = 45, Count = 1}, -- M14 Rifle
-  {Item = "Base.AssaultRifle", Number = 50, Count = 1}, -- M16 Assault Rifle
-  {Item = "Base.DoubleBarrelShotgun", Number = 55, Count = 1}, -- Double Barrel Shotgun
-  {Item = "Base.Bullets38", Number = 60, Count = 2}, -- .38 Special Round
-  {Item = "Base.ShotgunShellsBox", Number = 65, Count = 1}, -- Box of Shotgun Shells
-  {Item = "Base.308Box", Number = 70, Count = 1}, -- Box of .308 Rounds
-  {Item = "Base.556Box", Number = 75, Count = 1}, -- Box of 5.56mm Rounds
-  {Item = "Base.LongJohns", Number = 80, Count = 1}, -- Long Johns
-  {Item = "Base.Jumper_DiamondPatternTINT", Number = 85, Count = 1}, -- Diamond-pattern Sweater
-  {Item = "Base.Jacket_Black", Number = 90, Count = 1}, -- Leather Jacket
-  {Item = "Base.Apron_Spiffos", Number = 95, Count = 1}, -- Spiffo's Server Apron
-  {Item = "Base.TrousersMesh_Leather", Number = 100, Count = 1}, -- Skinny Leather Trousers
-  {Item = "Base.SpiffoSuit", Number = 105, Count = 1}, -- Spiffo Suit
-  {Item = "Base.SpiffoTail", Number = 110, Count = 1}, -- Spiffo Suit Tail
-  {Item = "Base.BunnyTail", Number = 115, Count = 1}, -- Bunny Tail
-  {Item = "Base.Hat_HockeyMask", Number = 120, Count = 1}, -- Hockey Mask
-  {Item = "Base.Hat_BunnyEarsBlack", Number = 125, Count = 1}, -- Bunny Ears (Black)
-  {Item = "Base.Hat_BunnyEarsWhite", Number = 130, Count = 1}, -- Bunny Ears (White)
-  {Item = "Base.Hat_Jay", Number = 135, Count = 1}, -- Jay Chicken Hat
-  {Item = "Base.Hat_Spiffo", Number = 140, Count = 1}, -- Spiffo Suit Head
-  {Item = "Base.Bag_ALICEpack_Army", Number = 145, Count = 1}, -- Military Backpack
-  {Item = "Base.Bag_SurvivorBag", Number = 150, Count = 1}, -- Large Backpack
-  {Item = "Base.Bag_ALICEpack", Number = 155, Count = 1}, -- Large Backpack
-  {Item = "Base.WaterMugSpiffo", Number = 160, Count = 1}, -- Mug of Water
-  {Item = "Base.CarBatteryCharger", Number = 165, Count = 1}, -- Car Battery Charger
-  {Item = "Base.WoodAxe", Number = 170, Count = 1}, -- Wood Axe
-  {Item = "Base.BoxOfJars", Number = 175, Count = 1}, -- Box of Jars
-  {Item = "Base.CombinationPadlock", Number = 180, Count = 1}, -- Combination Padlock
-  {Item = "Base.Padlock", Number = 185, Count = 1}, -- Padlock
-  {Item = "Base.BurgerRecipe", Number = 190, Count = 1}, -- Burger
-  {Item = "Base.AlcoholBandage", Number = 195, Count = 1}, -- Sterilized Bandage
-  {Item = "Base.Comfrey", Number = 200, Count = 2}, -- Comfrey
-  {Item = "Base.Ginseng", Number = 205, Count = 1}, -- Ginseng
-  {Item = "Radio.RadioMag1", Number = 210, Count = 1}, -- Guerilla Radio Vol. 1
-  {Item = "Radio.RadioMag2", Number = 215, Count = 1}, -- Guerilla Radio Vol. 2
-  {Item = "Radio.RadioMag3", Number = 220, Count = 1}, -- Guerilla Radio Vol. 3
-  {Item = "Base.HottieZ", Number = 225, Count = 1}, -- HottieZ
-  {Item = "Base.EngineParts", Number = 230, Count = 1}, -- Spare Engine Parts
-  {Item = "Base.Glue", Number = 235, Count = 1}, -- Glue
-  {Item = "Base.Woodglue", Number = 240, Count = 1}, -- Wood Glue
-  {Item = "Base.Aluminum", Number = 245, Count = 1}, -- Aluminum
-  {Item = "Base.SheetMetal", Number = 250, Count = 1}, -- Metal Sheet
-  {Item = "Base.Pillow", Number = 255, Count = 1}, -- Pillow
-  {Item = "Base.PropaneTank", Number = 260, Count = 1}, -- Propane Tank
-  {Item = "Base.SmallSheetMetal", Number = 265, Count = 1}, -- Small Metal Sheet
-  {Item = "Radio.ElectricWire", Number = 270, Count = 1}, -- Electric Wire
-  {Item = "Base.SpiffoBig", Number = 275, Count = 1}, -- Big Spiffo
-  {Item = "Base.Spiffo", Number = 280, Count = 1}, -- Spiffo
-  {Item = "Base.VHS_Home", Number = 290, Count = 1}, -- VHS Home
+    {Item = "Base.BaseballBatNails", Number = 5, Count = 1, zkills = 1000}, -- Spiked Baseball Bat
+    {Item = "Base.FishingRodTwineLine", Number = 10, Count = 1, zkills = 500}, -- Fishing Rod
+    {Item = "Base.MetalBar", Number = 15, Count = 3, zkills = 500}, -- Metal Bar
+    {Item = "Base.MetalPipe", Number = 20, Count = 3, zkills = 500}, -- Metal Pipe
+    {Item = "Base.Katana", Number = 25, Count = 1, zkills = 3000}, -- Katana
+    {Item = "Base.Machete", Number = 30, Count = 1, zkills = 2000}, -- Machete
+    {Item = "Base.GardenFork", Number = 35, Count = 1, zkills = 500}, -- Garden Fork
+    {Item = "Base.Revolver_Short", Number = 40, Count = 1, zkills = 500}, -- M36 Revolver
+    {Item = "Base.AssaultRifle2", Number = 45, Count = 1, zkills = 3000}, -- M14 Rifle
+    {Item = "Base.AssaultRifle", Number = 50, Count = 1, zkills = 3000}, -- M16 Assault Rifle
+    {Item = "Base.DoubleBarrelShotgun", Number = 55, Count = 1, zkills = 1000}, -- Double Barrel Shotgun
+    {Item = "Base.Bullets38", Number = 60, Count = 2, zkills = 500}, -- .38 Special Round
+    {Item = "Base.ShotgunShellsBox", Number = 65, Count = 1, zkills = 500}, -- Box of Shotgun Shells
+    {Item = "Base.308Box", Number = 70, Count = 1, zkills = 500}, -- Box of .308 Rounds
+    {Item = "Base.556Box", Number = 75, Count = 1, zkills = 500}, -- Box of 5.56mm Rounds
+    {Item = "Base.LongJohns", Number = 80, Count = 1, zkills = 2000}, -- Long Johns
+    {Item = "Base.Jumper_DiamondPatternTINT", Number = 85, Count = 1, zkills = 1000}, -- Diamond-pattern Sweater
+    {Item = "Base.Jacket_Black", Number = 90, Count = 1, zkills = 2000}, -- Leather Jacket
+    {Item = "Base.Apron_Spiffos", Number = 95, Count = 1, zkills = 1000}, -- Spiffo's Server Apron
+    {Item = "Base.TrousersMesh_Leather", Number = 100, Count = 1, zkills = 1000}, -- Skinny Leather Trousers
+    {Item = "Base.SpiffoSuit", Number = 105, Count = 1, zkills = 3000}, -- Spiffo Suit
+    {Item = "Base.SpiffoTail", Number = 110, Count = 1, zkills = 5000}, -- Spiffo Suit Tail
+    {Item = "Base.BunnyTail", Number = 115, Count = 1, zkills = 500}, -- Bunny Tail
+    {Item = "Base.Hat_HockeyMask", Number = 120, Count = 1, zkills = 500}, -- Hockey Mask
+    {Item = "Base.Hat_BunnyEarsBlack", Number = 125, Count = 1, zkills = 500}, -- Bunny Ears (Black)
+    {Item = "Base.Hat_BunnyEarsWhite", Number = 130, Count = 1, zkills = 500}, -- Bunny Ears (White)
+    {Item = "Base.Hat_Jay", Number = 135, Count = 1, zkills = 500}, -- Jay Chicken Hat
+    {Item = "Base.Hat_Spiffo", Number = 140, Count = 1, zkills = 10000}, -- Spiffo Suit Head
+    {Item = "Base.Bag_ALICEpack_Army", Number = 145, Count = 1, zkills = 3000}, -- Military Backpack
+    {Item = "Base.Bag_SurvivorBag", Number = 150, Count = 1, zkills = 3000}, -- Large Backpack
+    {Item = "Base.Bag_ALICEpack", Number = 155, Count = 1, zkills = 3000}, -- Large Backpack
+    {Item = "Base.MugSpiffo", Number = 160, Count = 1, zkills = 500}, -- Empty Spiffo Mug
+    {Item = "Base.CarBatteryCharger", Number = 165, Count = 1, zkills = 1000}, -- Car Battery Charger
+    {Item = "Base.WoodAxe", Number = 170, Count = 1, zkills = 3000}, -- Wood Axe
+    {Item = "Base.BoxOfJars", Number = 175, Count = 1, zkills = 1000}, -- Box of Jars
+    {Item = "Base.CombinationPadlock", Number = 180, Count = 1, zkills = 500}, -- Combination Padlock
+    {Item = "Base.Padlock", Number = 185, Count = 1, zkills = 500}, -- Padlock
+    {Item = "Base.BurgerRecipe", Number = 190, Count = 1, zkills = 200}, -- Burger
+    {Item = "Base.AlcoholBandage", Number = 195, Count = 1, zkills = 500}, -- Sterilized Bandage
+    {Item = "Base.Comfrey", Number = 200, Count = 2, zkills = 500}, -- Comfrey
+    {Item = "Base.Ginseng", Number = 205, Count = 1, zkills = 200}, -- Ginseng
+    {Item = "Radio.RadioMag1", Number = 210, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 1
+    {Item = "Radio.RadioMag2", Number = 215, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 2
+    {Item = "Radio.RadioMag3", Number = 220, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 3
+    {Item = "Base.HottieZ", Number = 225, Count = 1, zkills = 1500}, -- HottieZ
+    {Item = "Base.EngineParts", Number = 230, Count = 1, zkills = 1000}, -- Spare Engine Parts
+    {Item = "Base.Glue", Number = 235, Count = 1, zkills = 150}, -- Glue
+    {Item = "Base.Woodglue", Number = 240, Count = 1, zkills = 500}, -- Wood Glue
+    {Item = "Base.Aluminum", Number = 245, Count = 1, zkills = 500}, -- Aluminum
+    {Item = "Base.SheetMetal", Number = 250, Count = 1, zkills = 500}, -- Metal Sheet
+    {Item = "Base.Pillow", Number = 255, Count = 1, zkills = 500}, -- Pillow
+    {Item = "Base.PropaneTank", Number = 260, Count = 1, zkills = 1000}, -- Propane Tank
+    {Item = "Base.SmallSheetMetal", Number = 265, Count = 1, zkills = 500}, -- Small Metal Sheet
+    {Item = "Radio.ElectricWire", Number = 270, Count = 1, zkills = 500}, -- Electric Wire
+    {Item = "Base.SpiffoBig", Number = 275, Count = 1, zkills = 3000}, -- Big Spiffo
+    {Item = "Base.Spiffo", Number = 280, Count = 1, zkills = 3000}, -- Spiffo
+    {Item = "Base.VHS_Home", Number = 290, Count = 1, zkills = 5000}, -- VHS Home
 }
 
 local function ZombKilled(zombie)
-  local attacker = zombie:getAttackedBy()
+    local attacker = zombie:getAttackedBy()
 
-  if attacker == nil or not instanceof(attacker, "IsoPlayer") or attacker:getVehicle() ~= nil then
-    return
-  end
-
-  local number = ZombRandBetween(1, 501)
-  local won = false
-
-  for key, value in pairs(itemsAwards) do
-    if (number == value.Number) then
-      local itemName = getItemNameFromFullType(value.Item)
-      zombie:getInventory():AddItems(value.Item, value.Count)
-      if Awards.Options.showMessageInChat then
-        attacker:Say(string.format(getText("IGUI_WonItem"), itemName, value.Count))
-      else
-        attacker:setHaloNote(string.format(getText("IGUI_WonItem"), itemName, value.Count))
-      end
-      won = true
-      break
+    if attacker == nil or not instanceof(attacker, "IsoPlayer") or attacker:getVehicle() ~= nil then
+        return
     end
-  end
 
-  if not won and Awards.Options.showNumberWhenLosing then
-    if Awards.Options.showMessageInChat then
-      attacker:Say(string.format(getText("IGUI_LoseItem"), number))
-    else
-      attacker:setHaloNote(string.format(getText("IGUI_LoseItem"), number), 255, 0, 0, 300)
+    local number = ZombRandBetween(1, 501)
+    local countZombieKill = attacker:getZombieKills() + 1
+    local won = false
+
+    for key, value in pairs(itemsAwards) do
+        if (number == value.Number) then
+            if (countZombieKill >= value.zkills) then
+                local itemName = getItemNameFromFullType(value.Item)
+                attacker:getInventory():AddItems(value.Item, value.Count)
+                local message = string.format(getText("IGUI_WonItem"), itemName, value.Count)
+
+                if Awards.Options.showMessageInChat then
+                    attacker:Say(message)
+                else
+                    attacker:setHaloNote(message)
+                end
+
+                local awardMessage = itemName .. " x" .. value.Count
+
+                if AddAwardsLogMessage then
+                    AddAwardsLogMessage(awardMessage)
+                end
+
+                -- Aquí pasamos el tipo de item y el mensaje para la UI
+                if AddAwardMessageToUI then
+                    AddAwardMessageToUI(value.Item, awardMessage)
+                end
+
+            else
+                local needMoreKillsMessage = string.format(getText("IGUI_YouNeedMoreKills"), number, value.zkills)
+                if Awards.Options.showMessageInChat then
+                    attacker:Say(needMoreKillsMessage)
+                else
+                    attacker:setHaloNote(needMoreKillsMessage, 255, 0, 0, 300)
+                end
+
+                if AddLoserMessageToUI then
+                    AddLoserMessageToUI(needMoreKillsMessage)
+                end
+            end
+
+            won = true
+            break
+        end
     end
-  end
+
+    if (not won) then
+        local message = string.format(getText("IGUI_LoseItem"), number)
+
+        if (Awards.Options.showNumberWhenLosing) then
+            if Awards.Options.showMessageInChat then
+                attacker:Say(message)
+            else
+                attacker:setHaloNote(message, 255, 0, 0, 300)
+            end
+        end
+
+        if AddLoserMessageToUI then
+            AddLoserMessageToUI(message)
+        end
+    end
 end
 
 Events.OnZombieDead.Add(ZombKilled)
