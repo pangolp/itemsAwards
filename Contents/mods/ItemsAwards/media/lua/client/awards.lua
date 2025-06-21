779b183247bf3b321e5e16a3ae4f2a4386f00928
Pagani Walter <paganiwalter@gmail.com>
2025-06-21 09:53:48 -0300

    Update and better organize awards

--- Contents/mods/ItemsAwards/media/lua/client/awards.lua
+++ Contents/mods/ItemsAwards/media/lua/client/awards.lua
@@ -2,80 +2,96 @@ Awards = Awards or {}
 Awards.Options = Awards.Options or {}
 
 local itemsAwards = {
-    {Item = "Base.BaseballBatNails", Number = 5, Count = 1, zkills = 1000}, -- Spiked Baseball Bat
-    {Item = "Base.FishingRodTwineLine", Number = 10, Count = 1, zkills = 500}, -- Fishing Rod
-    {Item = "Base.MetalBar", Number = 15, Count = 3, zkills = 500}, -- Metal Bar
-    {Item = "Base.MetalPipe", Number = 20, Count = 3, zkills = 500}, -- Metal Pipe
-    {Item = "Base.Katana", Number = 25, Count = 1, zkills = 3000}, -- Katana
-    {Item = "Base.Machete", Number = 30, Count = 1, zkills = 2000}, -- Machete
-    {Item = "Base.GardenFork", Number = 35, Count = 1, zkills = 500}, -- Garden Fork
-    {Item = "Base.Revolver_Short", Number = 40, Count = 1, zkills = 500}, -- M36 Revolver
-    {Item = "Base.AssaultRifle2", Number = 45, Count = 1, zkills = 3000}, -- M14 Rifle
-    {Item = "Base.AssaultRifle", Number = 50, Count = 1, zkills = 3000}, -- M16 Assault Rifle
-    {Item = "Base.DoubleBarrelShotgun", Number = 55, Count = 1, zkills = 1000}, -- Double Barrel Shotgun
-    {Item = "Base.Bullets38", Number = 60, Count = 2, zkills = 500}, -- .38 Special Round
-    {Item = "Base.ShotgunShellsBox", Number = 65, Count = 1, zkills = 500}, -- Box of Shotgun Shells
-    {Item = "Base.308Box", Number = 70, Count = 1, zkills = 500}, -- Box of .308 Rounds
-    {Item = "Base.556Box", Number = 75, Count = 1, zkills = 500}, -- Box of 5.56mm Rounds
-    {Item = "Base.LongJohns", Number = 80, Count = 1, zkills = 2000}, -- Long Johns
-    {Item = "Base.Jumper_DiamondPatternTINT", Number = 85, Count = 1, zkills = 1000}, -- Diamond-pattern Sweater
-    {Item = "Base.Jacket_Black", Number = 90, Count = 1, zkills = 2000}, -- Leather Jacket
-    {Item = "Base.Apron_Spiffos", Number = 95, Count = 1, zkills = 1000}, -- Spiffo's Server Apron
-    {Item = "Base.TrousersMesh_Leather", Number = 100, Count = 1, zkills = 1000}, -- Skinny Leather Trousers
-    {Item = "Base.SpiffoSuit", Number = 105, Count = 1, zkills = 3000}, -- Spiffo Suit
-    {Item = "Base.SpiffoTail", Number = 110, Count = 1, zkills = 5000}, -- Spiffo Suit Tail
-    {Item = "Base.BunnyTail", Number = 115, Count = 1, zkills = 500}, -- Bunny Tail
-    {Item = "Base.Hat_HockeyMask", Number = 120, Count = 1, zkills = 500}, -- Hockey Mask
-    {Item = "Base.Hat_BunnyEarsBlack", Number = 125, Count = 1, zkills = 500}, -- Bunny Ears (Black)
-    {Item = "Base.Hat_BunnyEarsWhite", Number = 130, Count = 1, zkills = 500}, -- Bunny Ears (White)
-    {Item = "Base.Hat_Jay", Number = 135, Count = 1, zkills = 500}, -- Jay Chicken Hat
-    {Item = "Base.Hat_Spiffo", Number = 140, Count = 1, zkills = 10000}, -- Spiffo Suit Head
-    {Item = "Base.Bag_ALICEpack_Army", Number = 145, Count = 1, zkills = 3000}, -- Military Backpack
-    {Item = "Base.Bag_SurvivorBag", Number = 150, Count = 1, zkills = 3000}, -- Large Backpack
-    {Item = "Base.Bag_ALICEpack", Number = 155, Count = 1, zkills = 3000}, -- Large Backpack
-    {Item = "Base.MugSpiffo", Number = 160, Count = 1, zkills = 500}, -- Empty Spiffo Mug
-    {Item = "Base.CarBatteryCharger", Number = 165, Count = 1, zkills = 10000}, -- Car Battery Charger
-    {Item = "Base.WoodAxe", Number = 170, Count = 1, zkills = 3000}, -- Wood Axe
-    {Item = "Base.BoxOfJars", Number = 175, Count = 1, zkills = 1000}, -- Box of Jars
-    {Item = "Base.CombinationPadlock", Number = 180, Count = 1, zkills = 500}, -- Combination Padlock
-    {Item = "Base.Padlock", Number = 185, Count = 1, zkills = 500}, -- Padlock
-    {Item = "Base.BurgerRecipe", Number = 190, Count = 1, zkills = 200}, -- Burger
-    {Item = "Base.AlcoholBandage", Number = 195, Count = 1, zkills = 500}, -- Sterilized Bandage
-    {Item = "Base.Comfrey", Number = 200, Count = 2, zkills = 500}, -- Comfrey
-    {Item = "Base.Ginseng", Number = 205, Count = 1, zkills = 200}, -- Ginseng
-    {Item = "Radio.RadioMag1", Number = 210, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 1
-    {Item = "Radio.RadioMag2", Number = 215, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 2
-    {Item = "Radio.RadioMag3", Number = 220, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 3
-    {Item = "Base.HottieZ", Number = 225, Count = 1, zkills = 1500}, -- HottieZ
-    {Item = "Base.EngineParts", Number = 230, Count = 1, zkills = 1000}, -- Spare Engine Parts
-    {Item = "Base.Glue", Number = 235, Count = 1, zkills = 150}, -- Glue
-    {Item = "Base.Woodglue", Number = 240, Count = 1, zkills = 500}, -- Wood Glue
-    {Item = "Base.Aluminum", Number = 245, Count = 1, zkills = 500}, -- Aluminum
-    {Item = "Base.SheetMetal", Number = 250, Count = 1, zkills = 500}, -- Metal Sheet
-    {Item = "Base.Pillow", Number = 255, Count = 1, zkills = 500}, -- Pillow
-    {Item = "Base.PropaneTank", Number = 260, Count = 1, zkills = 1000}, -- Propane Tank
-    {Item = "Base.SmallSheetMetal", Number = 265, Count = 1, zkills = 500}, -- Small Metal Sheet
-    {Item = "Radio.ElectricWire", Number = 270, Count = 1, zkills = 500}, -- Electric Wire
-    {Item = "Base.SpiffoBig", Number = 275, Count = 1, zkills = 3000}, -- Big Spiffo
-    {Item = "Base.Spiffo", Number = 280, Count = 1, zkills = 3000}, -- Spiffo
-    {Item = "Base.VHS_Home", Number = 290, Count = 1, zkills = 5000}, -- VHS Home
-    {Item = "Base.Salt", Number = 300, Count = 1, zkills = 5000}, -- Salt
-    {Item = "Base.TomatoPaste", Number = 305, Count = 1, zkills = 5000}, -- Tomato Paste
-    {Item = "Base.OilOlive", Number = 310, Count = 1, zkills = 5000}, -- Oil Olive
-    {Item = "Base.Cheese", Number = 315, Count = 1, zkills = 5000}, -- Cheese
-    {Item = "Base.Book", Number = 320, Count = 1, zkills = 10000}, -- Book
-    {Item = "Base.PaintBlack", Number = 325, Count = 1, zkills = 10000}, -- Paint Black
-    {Item = "Base.PaintBlue", Number = 330, Count = 1, zkills = 10000}, -- Paint Blue
-    {Item = "Base.PaintBrown", Number = 335, Count = 1, zkills = 10000}, -- Paint Brown
-    {Item = "Base.PaintCyan", Number = 340, Count = 1, zkills = 10000}, -- Paint Cyan
-    {Item = "Base.PaintGreen", Number = 345, Count = 1, zkills = 10000}, -- Paint Green
-    {Item = "Base.PaintGrey", Number = 350, Count = 1, zkills = 10000}, -- Paint Grey
-    {Item = "Base.PaintOrange", Number = 355, Count = 1, zkills = 10000}, -- Paint Orange
-    {Item = "Base.PaintPink", Number = 360, Count = 1, zkills = 10000}, -- Paint Pink
-    {Item = "Base.PaintPurple", Number = 365, Count = 1, zkills = 10000}, -- Paint Purple
-    {Item = "Base.PaintRed", Number = 370, Count = 1, zkills = 10000}, -- Paint Red
-    {Item = "Base.PaintWhite", Number = 375, Count = 1, zkills = 10000}, -- Paint White
-    {Item = "Base.PaintYellow", Number = 380, Count = 1, zkills = 10000}, -- Paint Yellow
+    -- Handguns
+    {Item = "Base.Pistol3", Number = 5, Count = 1, zkills = 1500}, -- D-E Pistol
+    {Item = "Base.Pistol2", Number = 10, Count = 1, zkills = 1500}, -- M1911 Pistol
+    {Item = "Base.Revolver_Short", Number = 15, Count = 1, zkills = 1500}, -- M36 Revolver
+    {Item = "Base.Revolver", Number = 20, Count = 1, zkills = 1500}, -- M625 Revolver
+    {Item = "Base.Pistol", Number = 25, Count = 1, zkills = 1500}, -- M9 Pistol
+    {Item = "Base.Revolver_Long", Number = 30, Count = 1, zkills = 1500}, -- Magnum
+    -- Shotguns
+    {Item = "Base.DoubleBarrelShotgun", Number = 35, Count = 1, zkills = 1500}, -- Double Barrel Shotgun
+    {Item = "Base.Shotgun", Number = 40, Count = 1, zkills = 1500}, -- JS-2000 Shotgun
+    {Item = "Base.DoubleBarrelShotgunSawnoff", Number = 45, Count = 1, zkills = 1500}, -- Sawed-off Double Barrel Shotgun
+    {Item = "Base.ShotgunSawnoff", Number = 50, Count = 1, zkills = 1500}, -- Sawed-off JS-2000 Shotgun
+    -- Rifles
+    {Item = "Base.AssaultRifle2", Number = 55, Count = 1, zkills = 2500}, -- M14 Rifle
+    {Item = "Base.AssaultRifle", Number = 60, Count = 1, zkills = 2500}, -- M16 Assault Rifle
+    {Item = "Base.VarmintRifle", Number = 65, Count = 1, zkills = 2500}, -- MSR700 Rifle
+    {Item = "Base.HuntingRifle", Number = 70, Count = 1, zkills = 2500}, -- MSR788 Rifle
+    -- Ammo (Boxes)
+    {Item = "Base.223Box", Number = 75, Count = 1, zkills = 3500}, -- Box of .223 Rounds
+    {Item = "Base.308Box", Number = 80, Count = 1, zkills = 3500}, -- Box of .308 Rounds
+    {Item = "Base.Bullets38Box", Number = 85, Count = 1, zkills = 3500}, -- Box of .38 Special Rounds
+    {Item = "Base.Bullets44Box", Number = 90, Count = 1, zkills = 3500}, -- Box of .44 Magnum Rounds
+    {Item = "Base.Bullets45Box", Number = 95, Count = 1, zkills = 3500}, -- Box of .45 Auto Rounds
+    {Item = "Base.556Box", Number = 100, Count = 1, zkills = 3500}, -- Box of 5.56mm Rounds
+    {Item = "Base.Bullets9mmBox", Number = 105, Count = 1, zkills = 3500}, -- Box of 9mm Rounds
+    {Item = "Base.ShotgunShellsBox", Number = 110, Count = 1, zkills = 3500}, -- Box of Shotgun Shells
+    -- Others
+    {Item = "Base.BaseballBatNails", Number = 115, Count = 1, zkills = 1000}, -- Spiked Baseball Bat
+    {Item = "Base.FishingRodTwineLine", Number = 120, Count = 1, zkills = 500}, -- Fishing Rod
+    {Item = "Base.MetalBar", Number = 125, Count = 3, zkills = 500}, -- Metal Bar
+    {Item = "Base.MetalPipe", Number = 130, Count = 3, zkills = 500}, -- Metal Pipe
+    {Item = "Base.Katana", Number = 135, Count = 1, zkills = 3000}, -- Katana
+    {Item = "Base.Machete", Number = 140, Count = 1, zkills = 2000}, -- Machete
+    {Item = "Base.GardenFork", Number = 145, Count = 1, zkills = 500}, -- Garden Fork
+    {Item = "Base.LongJohns", Number = 150, Count = 1, zkills = 2000}, -- Long Johns
+    {Item = "Base.Jumper_DiamondPatternTINT", Number = 155, Count = 1, zkills = 1000}, -- Diamond-pattern Sweater
+    {Item = "Base.Jacket_Black", Number = 160, Count = 1, zkills = 2000}, -- Leather Jacket
+    {Item = "Base.Apron_Spiffos", Number = 165, Count = 1, zkills = 1000}, -- Spiffo's Server Apron
+    {Item = "Base.TrousersMesh_Leather", Number = 170, Count = 1, zkills = 1000}, -- Skinny Leather Trousers
+    {Item = "Base.SpiffoSuit", Number = 175, Count = 1, zkills = 3000}, -- Spiffo Suit
+    {Item = "Base.SpiffoTail", Number = 180, Count = 1, zkills = 5000}, -- Spiffo Suit Tail
+    {Item = "Base.BunnyTail", Number = 185, Count = 1, zkills = 500}, -- Bunny Tail
+    {Item = "Base.Hat_HockeyMask", Number = 190, Count = 1, zkills = 500}, -- Hockey Mask
+    {Item = "Base.Hat_BunnyEarsBlack", Number = 195, Count = 1, zkills = 500}, -- Bunny Ears (Black)
+    {Item = "Base.Hat_BunnyEarsWhite", Number = 200, Count = 1, zkills = 500}, -- Bunny Ears (White)
+    {Item = "Base.Hat_Jay", Number = 205, Count = 1, zkills = 500}, -- Jay Chicken Hat
+    {Item = "Base.Hat_Spiffo", Number = 210, Count = 1, zkills = 10000}, -- Spiffo Suit Head
+    {Item = "Base.Bag_ALICEpack_Army", Number = 215, Count = 1, zkills = 3000}, -- Military Backpack
+    {Item = "Base.Bag_SurvivorBag", Number = 220, Count = 1, zkills = 3000}, -- Large Backpack
+    {Item = "Base.Bag_ALICEpack", Number = 225, Count = 1, zkills = 3000}, -- Large Backpack
+    {Item = "Base.MugSpiffo", Number = 230, Count = 1, zkills = 500}, -- Empty Spiffo Mug
+    {Item = "Base.CarBatteryCharger", Number = 235, Count = 1, zkills = 10000}, -- Car Battery Charger
+    {Item = "Base.WoodAxe", Number = 240, Count = 1, zkills = 3000}, -- Wood Axe
+    {Item = "Base.BoxOfJars", Number = 245, Count = 1, zkills = 1000}, -- Box of Jars
+    {Item = "Base.Padlock", Number = 250, Count = 1, zkills = 500}, -- Padlock
+    {Item = "Base.BurgerRecipe", Number = 255, Count = 1, zkills = 200}, -- Burger
+    {Item = "Base.AlcoholBandage", Number = 260, Count = 1, zkills = 500}, -- Sterilized Bandage
+    {Item = "Base.Comfrey", Number = 265, Count = 2, zkills = 500}, -- Comfrey
+    {Item = "Base.Ginseng", Number = 270, Count = 1, zkills = 200}, -- Ginseng
+    {Item = "Radio.RadioMag1", Number = 275, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 1
+    {Item = "Radio.RadioMag2", Number = 280, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 2
+    {Item = "Radio.RadioMag3", Number = 285, Count = 1, zkills = 1500}, -- Guerilla Radio Vol. 3
+    {Item = "Base.HottieZ", Number = 290, Count = 1, zkills = 1500}, -- HottieZ
+    {Item = "Base.EngineParts", Number = 295, Count = 1, zkills = 1000}, -- Spare Engine Parts
+    {Item = "Base.Glue", Number = 300, Count = 1, zkills = 150}, -- Glue
+    {Item = "Base.Woodglue", Number = 305, Count = 1, zkills = 500}, -- Wood Glue
+    {Item = "Base.Aluminum", Number = 310, Count = 1, zkills = 500}, -- Aluminum
+    {Item = "Base.SheetMetal", Number = 315, Count = 1, zkills = 500}, -- Metal Sheet
+    {Item = "Base.Pillow", Number = 320, Count = 1, zkills = 500}, -- Pillow
+    {Item = "Base.PropaneTank", Number = 325, Count = 1, zkills = 1000}, -- Propane Tank
+    {Item = "Base.SmallSheetMetal", Number = 330, Count = 1, zkills = 500}, -- Small Metal Sheet
+    {Item = "Radio.ElectricWire", Number = 335, Count = 1, zkills = 500}, -- Electric Wire
+    {Item = "Base.VHS_Home", Number = 340, Count = 1, zkills = 5000}, -- VHS Home
+    {Item = "Base.Salt", Number = 345, Count = 1, zkills = 5000}, -- Salt
+    {Item = "Base.TomatoPaste", Number = 350, Count = 1, zkills = 5000}, -- Tomato Paste
+    {Item = "Base.OilOlive", Number = 355, Count = 1, zkills = 5000}, -- Oil Olive
+    {Item = "Base.Cheese", Number = 360, Count = 1, zkills = 5000}, -- Cheese
+    {Item = "Base.Book", Number = 365, Count = 1, zkills = 10000}, -- Book
+    {Item = "Base.PaintBlack", Number = 370, Count = 1, zkills = 10000}, -- Paint Black
+    {Item = "Base.PaintBlue", Number = 375, Count = 1, zkills = 10000}, -- Paint Blue
+    {Item = "Base.PaintBrown", Number = 380, Count = 1, zkills = 10000}, -- Paint Brown
+    {Item = "Base.PaintCyan", Number = 385, Count = 1, zkills = 10000}, -- Paint Cyan
+    {Item = "Base.PaintGreen", Number = 390, Count = 1, zkills = 10000}, -- Paint Green
+    {Item = "Base.PaintGrey", Number = 395, Count = 1, zkills = 10000}, -- Paint Grey
+    {Item = "Base.PaintOrange", Number = 400, Count = 1, zkills = 10000}, -- Paint Orange
+    {Item = "Base.PaintPink", Number = 405, Count = 1, zkills = 10000}, -- Paint Pink
+    {Item = "Base.PaintPurple", Number = 410, Count = 1, zkills = 10000}, -- Paint Purple
+    {Item = "Base.PaintRed", Number = 415, Count = 1, zkills = 10000}, -- Paint Red
+    {Item = "Base.PaintWhite", Number = 420, Count = 1, zkills = 10000}, -- Paint White
+    {Item = "Base.PaintYellow", Number = 425, Count = 1, zkills = 10000}, -- Paint Yellow
 }
 
 local function ZombKilled(zombie)
